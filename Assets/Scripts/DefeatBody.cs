e2647e6f274815c50d5f66739184691f2b277d34
Patrick <prm5983@rit.edu>
2020-08-07 15:08:41 -0400

    Added lose and win conditions

---
+++ Assets/Scripts/DefeatBody.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefeatBody : MonoBehaviour
+{
+    private bool levelEnding = false;
+
+    private void Awake()
+    {
+        EventDispatcher.AddListener<EventDefiner.LevelEnd>(OnLevelEnd);
+    }
+    private void OnDestroy()
+    {
+        EventDispatcher.RemoveListener<EventDefiner.LevelEnd>(OnLevelEnd);
+    }
+    private void OnLevelEnd(EventDefiner.LevelEnd _) { levelEnding = true; }
+
+    //If the player touches this object, whether its a collider or trigger, make the level end in failure.
+    //Only do this if the level is not ending already.
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!levelEnding && other.CompareTag("Player"))
+        {
+            EventDispatcher.Dispatch(new EventDefiner.LevelEnd(false));
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!levelEnding && collision.gameObject.CompareTag("Player"))
+        {
+            EventDispatcher.Dispatch(new EventDefiner.LevelEnd(false));
+        }
+    }
+}
---
+++ Assets/Scripts/GoalTesseract.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalTesseract : MonoBehaviour
+{
+    [SerializeField] [Range(0f, 6f)] private float newRotationInterval = 3f;
+
+    private bool levelEnding = false;
+    private Quaternion lastRot;
+    private Quaternion destRot;
+    private float nextRotTimer = 0f;
+
+    private void Awake()
+    {
+        EventDispatcher.AddListener<EventDefiner.LevelEnd>(OnLevelEnd);
+    }
+    private void OnDestroy()
+    {
+        EventDispatcher.RemoveListener<EventDefiner.LevelEnd>(OnLevelEnd);
+    }
+    private void OnLevelEnd(EventDefiner.LevelEnd _) { levelEnding = true; }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!levelEnding && other.CompareTag("Player"))
+        {
+            EventDispatcher.Dispatch(new EventDefiner.LevelEnd(true));
+        }
+    }
+
+    private void Start() { ResetRots(); }
+    private void Update()
+    {
+        nextRotTimer += Time.deltaTime / newRotationInterval;
+
+        if (nextRotTimer < 1)
+        {
+            transform.rotation = Quaternion.Lerp(lastRot, destRot, nextRotTimer);
+        }
+        else { ResetRots(); }
+    }
+
+    private void ResetRots()
+    {
+        nextRotTimer = 0;
+        lastRot = transform.rotation;
+        destRot = Random.rotation;
+    }
+}
