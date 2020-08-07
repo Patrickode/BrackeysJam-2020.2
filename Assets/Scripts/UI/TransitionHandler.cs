36ae86e713bfc8263943cab9cd300403e36cee4f
Patrick <prm5983@rit.edu>
2020-08-07 14:03:46 -0400

    Added a screen effect to rewinding

---
+++ Assets/Scripts/UI/TransitionHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransitionHandler : MonoBehaviour
+{
+    [SerializeField] private Animator spiralAnim = null;
+    [SerializeField] private Animator clockAnim = null;
+
+    private void Awake()
+    {
+        EventDispatcher.AddListener<EventDefiner.Rewind>(OnRewind);
+    }
+    private void OnDestroy()
+    {
+        EventDispatcher.RemoveListener<EventDefiner.Rewind>(OnRewind);
+    }
+
+    private void OnRewind(EventDefiner.Rewind evt)
+    {
+        spiralAnim.SetTrigger("Start");
+        clockAnim.SetTrigger("Start");
+    }
+}
