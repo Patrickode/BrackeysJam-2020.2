f50774efd5c6b42552063d788cdab078726777a4
Patrick <prm5983@rit.edu>
2020-08-07 21:05:50 -0400

    Finished Level 1 and 2

---
+++ Assets/Scripts/BreakableHandler.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakableHandler : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private Renderer rend;
+    [SerializeField] private Collider coll;
+    [SerializeField] private ParticleSystem pSystem = null;
+
+    [Header("Materials")]
+    [SerializeField] private Material onMaterial = null;
+    [SerializeField] private Material offMaterial = null;
+
+    private float maxScaleAxis = 0f;
+
+#if UNITY_EDITOR
+    [Space(10)]
+    [SerializeField] private bool toggleRendAndColl = false;
+
+    private void OnValidate()
+    {
+        if (toggleRendAndColl)
+        {
+            rend.material = !coll.enabled ? onMaterial : offMaterial;
+            coll.enabled = !coll.enabled;
+
+            toggleRendAndColl = false;
+        }
+    }
+#endif
+
+    private void Start()
+    {
+        Vector3 scale = transform.localScale;
+        maxScaleAxis = Mathf.Max(scale.x, scale.y);
+        maxScaleAxis = Mathf.Max(maxScaleAxis, scale.z);
+    }
+
+    public void ToggleAndBurst()
+    {
+        rend.material = !coll.enabled ? onMaterial : offMaterial;
+        coll.enabled = !coll.enabled;
+        pSystem.Emit(Mathf.CeilToInt(maxScaleAxis * 10));
+    }
+}
---
+++ Assets/Scripts/ButtonHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonHandler : MonoBehaviour
+{
+    [SerializeField] private List<BreakableHandler> linkedBreakables;
+    [SerializeField] private float buttonCooldown = 0.5f;
+    private Coroutine cooldownCor = null;
+    private bool coolingDown = false;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!coolingDown && collision.gameObject.CompareTag("Player"))
+        {
+            foreach (BreakableHandler breakable in linkedBreakables)
+            {
+                breakable.ToggleAndBurst();
+            }
+
+            if (cooldownCor != null) { StopCoroutine(cooldownCor); }
+            cooldownCor = StartCoroutine(ButtonCooldown(buttonCooldown));
+        }
+    }
+
+    private IEnumerator ButtonCooldown(float cooldownLength)
+    {
+        coolingDown = true;
+        yield return new WaitForSeconds(cooldownLength);
+        coolingDown = false;
+    }
+}
